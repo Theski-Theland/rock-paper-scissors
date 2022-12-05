e5658682aba7dd5f8b76c242704db0632616c46a
Robert Spennato <spennato.robert@gmail.com>
2022-12-05 13:10:42 -0500

    Add variables for user and computer generated choices and comments for code and functions

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
-    const answer = prompt("Choose! Rock, Paper, or Scissors?");
+    const answer = prompt("Choose! Rock, Paper, or Scissors?"); //prompts use to choose one of three options
 
-    let playerSelection = (function(x) {
+    function capitalizeAnswer(x) {
     let first = x.charAt(0).toUpperCase();
     let rest = x.slice(1).toLowerCase();
     return first+rest;
-    })(answer);
+    }; //function to format users response to have first letter be a capital letter
     
     function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
     return choice = options[Math.floor(Math.random()*options.length)];
-    };
+    };//function to generate a random choice between Rock Paper and Scissors
+
+    let computerSelection = getComputerChoice();
+    let playerSelection = capitalizeAnswer(answer);
+    //assigns the users choice and computers choice to separate variables
+
+    
