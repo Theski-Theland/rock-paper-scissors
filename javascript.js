0a9513405d8e19b0376c0fefc7e827129a6c2c3e
Robert Spennato <spennato.robert@gmail.com>
2022-12-05 14:28:47 -0500

    Add rock paper scissors game function with all possible outcomes

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-    const answer = prompt("Choose! Rock, Paper, or Scissors?"); //prompts use to choose one of three options
+    let answer = prompt("Choose! Rock, Paper, or Scissors?"); //prompts use to choose one of three options
 
     function capitalizeAnswer(x) {
     let first = x.charAt(0).toUpperCase();
@@ -15,4 +15,28 @@
     let playerSelection = capitalizeAnswer(answer);
     //assigns the users choice and computers choice to separate variables
 
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === "Rock" && computerSelection === "Paper") {
+            alert("Computer chose paper! You lose!");
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            alert("Computer chose scissors! You win!");
+        } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+            alert("Draw! Play again!");
+        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            alert("Computer chose scissors! You lose!");
+        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            alert("Computer chose rock! You win!");
+        } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+            alert("Draw! Play again!");
+        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+            alert("Computer chose rock! You lose!");
+        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            alert("Computer chose paper! You win!");
+        } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+            alert("Draw! Play again!");
+        } else {
+            alert("Error! Either that's not an option or you can't spell!")
+        }
+    } //Rock Paper Scissors game. Gives results for each possible outcome and an error if user enters an improper response
     
+    playRound(playerSelection,computerSelection);
