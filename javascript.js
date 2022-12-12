78bf0910ecfd59e8dc4d2b9cb9907ae447a4c55f
Robert Spennato <spennato.robert@gmail.com>
2022-12-12 15:40:36 -0500

    Updates

--- javascript.js
+++ javascript.js
@@ -4,39 +4,41 @@
     let first = x.charAt(0).toUpperCase();
     let rest = x.slice(1).toLowerCase();
     return first+rest;
-    }; //function to format users response to have first letter be a capital letter
+    };
+    console.log(capitalizeAnswer(answer));
+    //function to format users response to have first letter be a capital letter
     
     function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
     return choice = options[Math.floor(Math.random()*options.length)];
-    };//function to generate a random choice between Rock Paper and Scissors
+    };
+    console.log(getComputerChoice());
+    //function to generate a random choice between Rock Paper and Scissors
 
-    let computerSelection = getComputerChoice();
     let playerSelection = capitalizeAnswer(answer);
+    console.log(playerSelection);
+    
+    let computerSelection = choice;
+    console.log(computerSelection);
     //assigns the users choice and computers choice to separate variables
 
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "Rock" && computerSelection === "Paper") {
-            alert("Computer chose paper! You lose!");
-        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-            alert("Computer chose scissors! You win!");
-        } else if (playerSelection === "Rock" && computerSelection === "Rock") {
-            alert("Draw! Play again!");
-        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-            alert("Computer chose scissors! You lose!");
-        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-            alert("Computer chose rock! You win!");
-        } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-            alert("Draw! Play again!");
-        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-            alert("Computer chose rock! You lose!");
-        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-            alert("Computer chose paper! You win!");
-        } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-            alert("Draw! Play again!");
+
+    function playRound(x, y) {
+        if (x === "Rock" && y === "Paper" ||
+        x === "Paper" && y === "Scissors" ||
+        x === "Scissors" && y === "Rock") {
+            return ("You lose!");
+        } else if (x === "Paper" && y === "Rock" ||
+        x === "Scissors" && y === "Paper" ||
+        x === "Rock" && y === "Scissors") {
+            return ("You win!");
+        } else if (x === y) {
+            return ("It's a tie!");
         } else {
-            alert("Error! Either that's not an option or you can't spell!")
+            return ("Bad choice. Try again!")
         }
-    } //Rock Paper Scissors game. Gives results for each possible outcome and an error if user enters an improper response
-    
-    playRound(playerSelection,computerSelection);
+    };
+        //Rock Paper Scissors game. Gives results for each possible outcome and an error if user enters an improper response
+        let results = playRound();
+        playRound(playerSelection,computerSelection);
+        console.log(results);
