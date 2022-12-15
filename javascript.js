5f5276c78c8eaff3200e9f737c431685a99ca904
Robert Spennato <spennato.robert@gmail.com>
2022-12-15 10:40:29 -0500

    Clean up code and remove unnecessary definitions

--- javascript.js
+++ javascript.js
@@ -1,26 +1,17 @@
     let answer = prompt("Choose! Rock, Paper, or Scissors?"); //prompts use to choose one of three options
-
-    function capitalizeAnswer(x) {
-    let newAnswer = x.charAt(0).toUpperCase() + x.slice(1).toLowerCase();
-    return newAnswer;
+    
+    function capitalizeAnswer(z) {
+        return userSelection = z.charAt(0).toUpperCase() + z.slice(1).toLowerCase();
     };
     console.log(capitalizeAnswer(answer));
     //function to format users response to have first letter be a capital letter
     
     function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
-    return choice = options[Math.floor(Math.random()*options.length)];
+        return computerSelection = options[Math.floor(Math.random()*options.length)];
     };
     console.log(getComputerChoice());
     //function to generate a random choice between Rock Paper and Scissors
-
-    let userSelection = capitalizeAnswer(answer);
-    console.log(userSelection);
-    
-    let computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    //assigns the users choice and computers choice to separate variables
-
 
     function playRound(x, y) {
         if (x === "Rock" && y === "Paper" ||
@@ -40,5 +31,6 @@
     };
         /* Rock Paper Scissors game. Gives results for each possible outcome and
         an error if user enters an improper response */
+        
         let results = playRound(userSelection,computerSelection);
         console.log(results);
