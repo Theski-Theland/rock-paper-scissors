9d1186f616a488d697a2e13e755619375bc63eb5
Robert Spennato <spennato.robert@gmail.com>
2022-12-16 10:43:22 -0500

    Rework variable names and placement, add scoreboard and message

--- javascript.js
+++ javascript.js
@@ -1,34 +1,51 @@
-    let answer = prompt("Choose! Rock, Paper, or Scissors?"); //prompts use to choose one of three options
-    
-    function capitalizeAnswer(z) {
-        return userSelection = z[0].toUpperCase() + z.slice(1).toLowerCase();
+    let answer = prompt("Choose! Rock, Paper, or Scissors?"); 
+        //prompts user to choose one of three options
+
+    let userSelection;
+    let computerSelection;
+    let userScore = 0;
+    let computerScore = 0;
+        //create containers for users and computers choice and score
+
+    function capitalizeAnswer(x) {
+        return userSelection = x[0].toUpperCase() + x.slice(1).toLowerCase();
     };
     console.log(capitalizeAnswer(answer));
-    //function to format users response to have first letter be a capital letter
+        /*function to format users response to have first letter be a capital letter 
+    and assign the return to the correct variable. Logs function */
     
     function getComputerChoice() {
     let options = ["Rock", "Paper", "Scissors"];
         return computerSelection = options[Math.floor(Math.random()*options.length)];
     };
     console.log(getComputerChoice());
-    //function to generate a random choice between Rock Paper and Scissors
-
-    function playRound(x, y) {
-        if (x === "Rock" && y === "Paper" ||
-        x === "Paper" && y === "Scissors" ||
-        x === "Scissors" && y === "Rock") {
-            return ("You lose!");
-        } else if (x === "Paper" && y === "Rock" ||
-        x === "Scissors" && y === "Paper" ||
-        x === "Rock" && y === "Scissors") {
-            return ("You win!");
-        } else if (x === y) {
+        /*function to generate a random choice for the computer between Rock Paper and Scissors 
+    and assign the return to the correct variable. Logs function */
+    
+    function playRound(user, computer) {
+        if (user === "Rock" && computer === "Paper" ||
+        user === "Paper" && computer === "Scissors" ||
+        user === "Scissors" && computer === "Rock") {
+            computerScore++;
+            return "You lose!"
+        } else if (user === "Paper" && computer === "Rock" ||
+        user === "Scissors" && computer === "Paper" ||
+        user === "Rock" && computer === "Scissors") {
+            userScore++;
+            return "You win!"
+        } else if (user === computer) {
             return ("It's a tie!");
         } else {
             alert("Bad choice! Refresh and try again!");
         }
     };
-        /* Rock Paper Scissors game. Gives results for each possible outcome and
-        an error if user enters an improper response */
-        
-        console.log(playRound(userSelection,computerSelection));
+    console.log(playRound(userSelection,computerSelection));
+    /* Rock Paper Scissors game. Gives a message with the results and awards a point for
+    each possible outcome. Gives an error message if user enters an improper response.
+    Runs and logs one round. */
+
+    let computerScoreMessage = "Computer Score: " + computerScore;
+    let userScoreMessage = "Player Score: " + userScore;
+    console.log(userScoreMessage);
+    console.log(computerScoreMessage);
+        //Creates containers combining a message with the score of the round played. Logs results
