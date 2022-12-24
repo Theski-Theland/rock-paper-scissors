5d5c454c0e43bf6c2b5d7bd35c4c0f91d105b399
Robert Spennato <spennato.robert@gmail.com>
2022-12-24 14:03:33 -0500

    Gmae function with loop and alerts for each round and game results

--- javascript.js
+++ javascript.js
@@ -1,43 +1,65 @@
-    let answer = prompt("Choose! Rock, Paper, or Scissors?");
-    let userSelection = answer[0].toUpperCase() + answer.slice(1).toLowerCase();
-        /* Prompts user to choose between Rock, Paper, and Scissors then formats 
-        users answer to appear with first letter as a capital */
-    
-    let options = ["Rock", "Paper", "Scissors"];
-    let computerSelection = options[Math.floor(Math.random()*3)];
-        //Creates random choice for computer between Rock, Paper, and Scissors
-        
-    console.log(userSelection);
-    console.log(computerSelection);
-    
+function game() {
+
+    function computerChoice() {
+        let options = ["Rock", "Paper", "Scissors"];
+        let computerRandom = options[Math.floor(Math.random()*3)];
+        return computerRandom;
+    }
+    //Creates random choice for computer between Rock, Paper, and Scissors
+
     let userScore = 0;
     let computerScore = 0;
-        //create containers for users and computers score
+    let userSelection;
+    let computerSelection = computerChoice();
+    let answer;
 
-    function playRound(user, computer) {
-        if (user === "Rock" && computer === "Paper" ||
-        user === "Paper" && computer === "Scissors" ||
-        user === "Scissors" && computer === "Rock") {
-            computerScore++;
-            return "You lose!"
-        } else if (user === "Paper" && computer === "Rock" ||
-        user === "Scissors" && computer === "Paper" ||
-        user === "Rock" && computer === "Scissors") {
-            userScore++;
-            return "You win!"
-        } else if (user === computer) {
-            return ("It's a tie!");
-        } else {
-            alert("Bad choice! Refresh and try again!");
-        }
+    //Containers for different variables needed for the game
+    
+    for (i=0;i<5;i++){
+        answer = prompt("Choose! Rock, Paper, or Scissors?");
+        userSelection = answer[0].toUpperCase() + answer.slice(1).toLowerCase();
+        /* Prompts user to choose between Rock, Paper, and Scissors then formats 
+        users answer to appear with first letter as a capital */
+
+        computerChoice();
+        // Function call to generate computers choice for each round
+
+        console.log(userSelection);
+        console.log(computerSelection);
+
+        function playRound(user, computer) {
+            if (computer === "Paper" && user === "Rock" ||
+            computer === "Scissors" && user === "Paper" ||
+            computer === "Rock" && user === "Scissors")  {
+                computerScore++;
+                alert ("Computer chose " + computerSelection + "! You lost this round! The score is " + userScore + " to " + computerScore + ".");
+                return userScore + " to " + computerScore;
+            } else if (user === "Paper" && computer === "Rock" ||
+            user === "Scissors" && computer === "Paper" ||
+            user === "Rock" && computer === "Scissors") {
+                userScore++;
+                alert ("Computer chose " + computerSelection + "! You won this round! The score is "  + userScore + " to " + computerScore + ".");
+                return userScore + " to " + computerScore;
+            } else if (user === computer) {
+                alert ("Computer chose " + computerSelection + "! It's a tie! The score is " + userScore + " to " + computerScore + ".");
+                return userScore + " to " + computerScore;
+            } else {
+                alert("Bad choice! Refresh and try again!");
+            }
+        };
+        //Round of Rock, Paper, Scissors with results and messages for each possible outcome
+
+        console.log(playRound(userSelection,computerSelection));
+    }
+
+    if (userScore > computerScore) {
+        alert ("You won the game " + userScore + " to " + computerScore + "!");
+    } else if (computerScore > userScore) {
+        alert ("You lost the game " + computerScore + " to " + userScore + "!")
+    } else {
+        alert (userScore + " to " + computerScore + "! The game is a tie!")
     };
-    console.log(playRound(userSelection,computerSelection));
-        /* Rock Paper Scissors game. Gives a message with the results and awards a point for
-    each possible outcome. Gives an error message if user enters an improper response.
-    Runs and logs one round. */
-        
-    let computerScoreMessage = "Computer Score: " + computerScore;
-    let userScoreMessage = "Player Score: " + userScore;
-    console.log(userScoreMessage);
-    console.log(computerScoreMessage);
-        //Creates containers combining a message with the score of the round
+}
+
+console.log(game());
+
