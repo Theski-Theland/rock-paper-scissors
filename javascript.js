4d8393820bc77607548d61e46405da164c6cc4cd
Robert Spennato <spennato.robert@gmail.com>
2023-01-13 13:59:22 -0500

    Add playRound function with proper prompts

--- javascript.js
+++ javascript.js
@@ -1,20 +1,79 @@
 let rounds;
 let computerSelection;
 let userSelection;
-let userScore = 0;
-let computerScore = 0;
+let usersScore = 0;
+let computersScore = 0;
 
 function gameRounds(){
-  rounds = document.getElementById('user-input').value;
-  return rounds;
+  return rounds = document.getElementById('user-input').value;
 };
 //Function to pull users choice for how many rounds to play
 
 function randomizeComputerChoice() {
   let options = ["Rock", "Paper", "Scissors"];
-  return computerSelection = options[Math.floor(Math.random() * options.length)];
+  return options[Math.floor(Math.random() * options.length)];
 }
-randomizeComputerChoice();
-console.log(computerSelection);
 //Function to randomly select computers choice
 
+function chooseRock() {
+  userSelection = document.getElementById('rock').value;
+  computerSelection = randomizeComputerChoice();
+  playRound(userSelection,computerSelection);
+}
+
+function choosePaper() {
+  userSelection = document.getElementById('paper').value;
+  computerSelection = randomizeComputerChoice();
+  playRound(userSelection,computerSelection);
+}
+
+function chooseScissors() {
+  userSelection = document.getElementById('scissors').value;
+  computerSelection = randomizeComputerChoice();
+  playRound(userSelection,computerSelection);
+}
+
+function playRound(user,computer) {
+  if (rounds === undefined) {
+    alert("Choose rounds first");
+  }
+    else if (user === computer) {
+      alert("tie");
+    }
+    else if (
+      user === "Rock" && computer === "Scissors" ||
+      user ===  "Paper" && computer === "Rock" ||
+      user === "Scissors" && computer === "Paper"
+    ) {
+      usersScore++;
+      console.log(computerSelection);
+      console.log(userSelection);
+      console.log(computersScore);
+      console.log(usersScore);
+        if (usersScore === Math.ceil(rounds / 2) || computersScore === Math.ceil(rounds / 2)) {
+          alert("end game win function goes here");
+          usersScore = 0;
+          computersScore = 0;
+          rounds = undefined;
+        };
+      "results game message goes here";
+    }
+    else if (
+      computer === "Rock" && user === "Scissors" ||
+      computer ===  "Paper" && user === "Rock" ||
+      computer === "Scissors" && user === "Paper"
+    ) {
+      computersScore++;
+      console.log(computerSelection);
+      console.log(userSelection);
+      console.log(computersScore);
+      console.log(usersScore);
+        if (usersScore === Math.ceil(rounds / 2) || computersScore === Math.ceil(rounds / 2)) {
+          alert("end game win function goes here");
+          usersScore = 0;
+          computersScore = 0;
+          rounds = undefined;
+        };
+      "results game message goes here";
+    }
+};
